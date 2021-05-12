5a4b3302357b8c69fefc62c133793a1b02bb6fb9
shrey9993 <83954314+shrey9993@users.noreply.github.com>
2021-05-12 03:35:39 -0700

    Add files via upload

---
+++ project c-20.js
@@ -0,0 +1,41 @@
+var car,wall;
+var speed,weight;
+
+function setup(){
+  createCanvas(1600,400);
+
+  car=createSprite(50,200,50,50);
+wall=createSprite(1200,200,60,100);
+
+speed=random(55,90);
+weight=random(400,1500);
+
+car.velocityx=speed;
+}
+
+
+function draw (){
+
+  background("black");
+
+  if(wall.x-car.x<(car.widht+wall.widht)/2){
+car.velocityx=0;
+var deformation=0.5+weight+speed+speed22509;
+if(deformation>180) {
+  car.shapeColour="red";
+}
+
+
+if (deformation<180 && deformation>100) {
+  car.shapeColour="yellow";
+}
+
+if (deformation>100)  {
+  car.shapeColour="green";
+
+}
+
+
+
+drawSprites()  ;
+  }
